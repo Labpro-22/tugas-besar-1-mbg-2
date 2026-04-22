90c7825de39f73e0472692540c6b35114c1288c7
GeraldoA07 <geraldoartemius2005@gmail.com>
2026-04-22 23:00:39 +0700

    feat : Input handler

--- include/core/TurnController.hpp
+++ include/core/TurnController.hpp
@@ -8,30 +8,14 @@
 #include "BankruptcyController.hpp"
 #include "SaveLoader.hpp"
 #include "Dice.hpp"
+#include "InputHandler.hpp"
+#include "GameLogger.hpp"
 
 using namespace std;
 
-enum class Command {
-    LEMPAR_DADU,
-    ATUR_DADU,
-    CETAK_PAPAN,
-    CETAK_AKTA,
-    CETAK_PROPERTI,
-    GADAI,
-    TEBUS,
-    BANGUN,
-    GUNAKAN_KEMAMPUAN,
-    SIMPAN,
-    MUAT,
-    CETAK_LOG,
-    UNKNOWN_COMMAND
-};
-
 class TurnController
 {
-private: 
-    Command parseCommand(const std::string& cmdInput);
 public:
     bool checkWinCondition();
-    void executeAction(GameContext* context, EconomyController& eco, EffectController& eff, AuctionController& auc, BankruptcyController& bank, Dice& dice, SaveLoader& sl);
+    void executeAction(GameContext* context, EconomyController& eco, EffectController& eff, AuctionController& auc, BankruptcyController& bank, Dice& dice, SaveLoader& sl, InputHandler& input, GameLogger& logger);
 };
--- include/utils/InputHandler.hpp
+++ include/utils/InputHandler.hpp
@@ -1,7 +1,24 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
+
+enum class CommandType {
+    LEMPAR_DADU,
+    ATUR_DADU,
+    CETAK_PAPAN,
+    CETAK_AKTA,
+    CETAK_PROPERTI,
+    GADAI,
+    TEBUS,
+    BANGUN,
+    GUNAKAN_KEMAMPUAN,
+    SIMPAN,
+    MUAT,
+    CETAK_LOG,
+    UNKNOWN_COMMAND
+};
 
 class InputHandler
 {
@@ -10,9 +27,16 @@ private:
     string lastStringInput;
     int valInt1;
     int valInt2;
+    static const map<string, CommandType> commandMap;
 public:
+    InputHandler(istream& input = cin);
+    CommandType getCommand();
     void getIntInput();
     void getStringInput();
     void clearInputBuffer();
+
+    string getLastStringInput() const;
+    int getIntValue1() const;
+    int getIntValue2() const;
 };
 
